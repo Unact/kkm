ead861999981b78e19f9f4e242b41525c3451e06
sudakov <vsudakov@bk.ru>
2017-02-16 19:02:29 +0300

    update example

--- test_ffi.rb
+++ test_ffi.rb
@@ -14,46 +14,91 @@ module FFIKkm
   extend FFI::Library
 
   ffi_lib "./bins/linux-x64/libKkm.so"
-  
+  enum :Alignment, [:AlignmentLeft, 0,
+              :AlignmentCenter,
+              :AlignmentRight
+  ]
+  enum :TextWrap, [ :TextWrapNone, 0, 
+                   :TextWrapWord,        
+                   :TextWrapLine
+  ]
+  # Типы / состояния чека
+  enum :ChequeType, [
+    :ChequeClosed, 0,            # Чек закрыт                               
+    :ChequeSell,                 # Чек продажи / прихода                    
+    :ChequeSellReturn,           # Чек возврат продажи / прихода            
+    :ChequeSellAnnulate,         # Чек аннулирования продажи                
+    :ChequeBuy,                  # Чек покупки / расхода                    
+    :ChequeBuyReturn,            # Чек возврата покупки / расхода           
+    :ChequeBuyAnnulate,          # Чек аннулирования покупки                
+    :ChequeSellCorrection,       # Чек коррекции прихода                    
+    :ChequeSellReturnCorrection, # Чек коррекции возврата прихода           
+    :ChequeBuyCorrection,        # Чек коррекции расхода                    
+    :ChequeBuyReturnCorrection,  # Чек коррекции возврата расхода           
+  ]
   attach_function(:put_DeviceSingleSetting, [:pointer, :string, :string], :int)
   attach_function(:put_DeviceSettings, [:pointer, :string], :int)
   attach_function(:get_DeviceSettings, [:pointer, :pointer, :int], :int)
   attach_function(:checkError, [:pointer], :void)
   attach_function(:ApplySingleSettings, [:pointer], :int)
   attach_function(:printFooter, [:pointer], :void)
-  attach_function(:put_DeviceEnabled, [:pointer], :int)
+  attach_function(:put_DeviceEnabled, [:pointer, :int], :int)
+  attach_function(:GetStatus, [:pointer], :int)
+  attach_function(:CancelCheck, [:pointer], :int)
+  attach_function(:printText,[:pointer, :string, :Alignment, :TextWrap], :void)
+  attach_function(:openCheck,[:pointer, :ChequeType], :void)
+  attach_function(:closeCheck,[:pointer, :int], :void)
+  attach_function(:registrationFZ54, [:pointer, :string, :double, :double, :int], :void)
+  attach_function(:payment, [:pointer, :double, :int], :void)
 end
 
 ifptr = FFIFptr.CreateFptrInterface(11)
 p ifptr
 
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "AccessPassword".force_encoding("UTF-16LE"), "0".force_encoding("UTF-16LE"))
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "BaudRate", "9600")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "Bits", "8")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "ConnectionType", "1")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "IPAddress", "192.168.101.64")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "IPPort", "5555")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "Model", "63")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "OfdPort", "NONE")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "Parity", "0")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "Port", "TCPIP")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "Protocol", "0")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "StopBits", "0")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "TTYSuffix", "ttyACM0")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "UserPassword", "30")
-res = FFIKkm.put_DeviceSingleSetting(ifptr, "ttyACM0", "63")
-res = FFIKkm.ApplySingleSettings(ifptr)
-p res
-# FFIKkm.checkError(ifptr)
+res = FFIKkm.put_DeviceSettings(ifptr, '
+<?xml version="1.0" encoding="UTF-8"?>
+<settings version="5">
+    <value name="AccessPassword">0</value>
+    <value name="AutoDisableBluetooth">0</value>
+    <value name="AutoEnableBluetooth">1</value>
+    <value name="BaudRate">9600</value>
+    <value name="Bits">8</value>
+    <value name="ConnectionType">1</value>
+    <value name="DeviceName">Device #1</value>
+    <value name="IPAddress">192.168.101.64</value>
+    <value name="IPPort">5555</value>
+    <value name="MACAddress"></value>
+    <value name="Model">63</value>
+    <value name="OfdPort">NONE</value>
+    <value name="Parity">0</value>
+    <value name="Port">TCPIP</value>
+    <value name="Protocol">0</value>
+    <value name="StopBits">0</value>
+    <value name="TTYSuffix">ttyACM0</value>
+    <value name="UserPassword">30</value>
+    <value name="ttyACM0">63</value>
+</settings>')
+
+buf = FFI::MemoryPointer.from_string( ' ' * 2000 )
 
 
-buf = FFI::MemoryPointer.from_string(' '*2000)
 res = FFIKkm.get_DeviceSettings(ifptr, buf, buf.size)
 res = buf.read_string(2000)
 puts res.strip
-p ifptr
-res = FFIKkm.put_DeviceEnabled(ifptr)
-res = FFIKkm.printFooter(ifptr)
+res = FFIKkm.put_DeviceEnabled(ifptr,1)
+res = FFIKkm.GetStatus(ifptr)
+res = FFIKkm.CancelCheck(ifptr)
+# FFIKkm.printText(ifptr,"Здесь был Вова!", :AlignmentLeft, :TextWrapWord)
+# FFIKkm.printFooter(ifptr)
+
+FFIKkm.openCheck(ifptr, :ChequeSell)
+
+
+FFIKkm.registrationFZ54(ifptr, "Позиция KuKu", 1.2, 66.0, 3)
+FFIKkm.registrationFZ54(ifptr, "Шпроты в масле", 1.13, 2.0, 3)
+FFIKkm.payment(ifptr, (1.2*66.0 + 1.13*2.0), 0)
+
+FFIKkm.closeCheck(ifptr, 0)
 
 puts "Ok"
 
