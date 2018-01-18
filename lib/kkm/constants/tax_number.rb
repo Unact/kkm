44c441a435dfb47e869c2d8fa9ba8c16c0fa2b7f
DCrow <y.barkovskij@unact.ru>
2018-01-18 11:33:15 +0300

    DEV-35197

--- lib/kkm/constants/tax_number.rb
+++ lib/kkm/constants/tax_number.rb
@@ -3,7 +3,7 @@ module Kkm::Constants::TaxNumber
   TAX_10      = 2   # НДС 10%
   TAX_18      = 3   # НДС 18%
   TAX_NONE    = 4   # БЕЗ НДС
-  TAX_10_110  = 5   # НДС 10/100
+  TAX_10_110  = 5   # НДС 10/110
   TAX_18_118  = 6   # НДС 18/118
 
   def self.tax_number_by_tax tax
