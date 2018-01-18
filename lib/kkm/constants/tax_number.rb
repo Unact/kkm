30a124ecb4272bcd20182050d27efd39351d3f88
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2018-01-18 11:37:08 +0300

    DEV-35197 (#12)

--- lib/kkm/constants/tax_number.rb
+++ lib/kkm/constants/tax_number.rb
@@ -3,11 +3,11 @@ module Kkm::Constants::TaxNumber
   TAX_10      = 2   # НДС 10%
   TAX_18      = 3   # НДС 18%
   TAX_NONE    = 4   # БЕЗ НДС
-  TAX_10_100  = 5   # НДС 10/100
+  TAX_10_110  = 5   # НДС 10/110
   TAX_18_118  = 6   # НДС 18/118
 
   def self.tax_number_by_tax tax
-    case tax
+    case tax.to_sym
     when :'0'
       TAX_0
     when :'10'
@@ -15,7 +15,7 @@ module Kkm::Constants::TaxNumber
     when :'18'
       TAX_18
     when :'10/110'
-      TAX_10_100
+      TAX_10_110
     when :'18/118'
       TAX_18_118
     else
